1d37c36a6e6d9ecd2840ec348a5345b4aebb3da3
Saydi Truong <truong@ionmobility.com>
2023-03-10 10:24:02 +0700

    fix compiler

--- host/linux/host_control/c_support/test.c
+++ host/linux/host_control/c_support/test.c
@@ -95,10 +95,10 @@ static int parse_cli_cmd(char *in_cmd, char *args[])
 		test_wifi_get_curr_tx_power();
 
 	/* Wifi Smart Connect APIs */
-	else if (0 == strncasecmp(SMARTCONNECT_START, in_cmd, sizeof(SMARTCONNECT_START)))
-		test_config_smartconnect();
-	else if (0 == strncasecmp(SMARTCONNECT_STOP, in_cmd, sizeof(SMARTCONNECT_STOP)))
-		test_disable_smartconnect();
+	// else if (0 == strncasecmp(SMARTCONNECT_START, in_cmd, sizeof(SMARTCONNECT_START)))
+	// 	test_config_smartconnect();
+	// else if (0 == strncasecmp(SMARTCONNECT_STOP, in_cmd, sizeof(SMARTCONNECT_STOP)))
+	// 	test_disable_smartconnect();
 	else if (0 == strncasecmp(IONHANDLE_START, in_cmd, sizeof(IONHANDLE_START)))
 		printf("ION ESP32 Wifi Handler Started...\n");
 	/* OTA ESP flashing */
