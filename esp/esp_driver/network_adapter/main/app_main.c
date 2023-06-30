3e140a66d79306605fa373ca5b357412c0d58eea
vult0306 <letuanvu91@gmail.com>
2023-07-01 00:12:54 +0700

    remove wifi source code to save flash memory

--- esp/esp_driver/network_adapter/main/app_main.c
+++ esp/esp_driver/network_adapter/main/app_main.c
@@ -56,10 +56,10 @@
 
 static const char TAG[] = "ION_BLE";
 
-#if CONFIG_ESP_WLAN_DEBUG
-static const char TAG_RX[] = "H -> S";
-static const char TAG_TX[] = "S -> H";
-#endif
+// #if CONFIG_ESP_WLAN_DEBUG
+// static const char TAG_RX[] = "H -> S";
+// static const char TAG_TX[] = "S -> H";
+// #endif
 
 #if CONFIG_ESP_SERIAL_DEBUG
 static const char TAG_RX_S[] = "CONTROL H -> S";
@@ -87,18 +87,12 @@ static const char TAG_TX_S[] = "CONTROL S -> H";
 
 volatile uint8_t action = 0;
 volatile uint8_t datapath = 0;
-volatile uint8_t station_connected = 0;
-volatile uint8_t smartconnect_started = 0;
-volatile uint8_t softap_started = 0;
-volatile uint8_t ota_ongoing = 0;
 
 interface_context_t *if_context = NULL;
 interface_handle_t *if_handle = NULL;
 
 static QueueHandle_t meta_to_host_queue = NULL;
 static QueueHandle_t to_host_queue[MAX_PRIORITY_QUEUES] = {NULL};
-
-
 static protocomm_t *pc_pserial;
 
 static struct rx_data {
@@ -107,8 +101,6 @@ static struct rx_data {
 	int len;
 	uint8_t data[4096];
 } r;
-
-uint8_t ap_mac[MAC_LEN] = {0};
 
 static void print_firmware_version()
 {
@@ -119,11 +111,6 @@ static void print_firmware_version()
 #else
 	ESP_LOGI(TAG, "                Transport used :: SDIO                          ");
 #endif
-#ifdef CONFIG_ESP_SMART_CONFIG_ENABLE
-	ESP_LOGI(TAG, "                Smart-Config   :: ENABLE                        ");
-#else
-	ESP_LOGI(TAG, "                Smart-Config   :: DISABLE                       ");
-#endif
 	ESP_LOGI(TAG, "*********************************************************************");
 }
 
@@ -132,13 +119,6 @@ static uint8_t get_capabilities()
 	uint8_t cap = 0;
 
 	ESP_LOGI(TAG, "Supported features are:");
-#if CONFIG_ESP_SPI_HOST_INTERFACE
-	ESP_LOGI(TAG, "- WLAN over SPI");
-	cap |= ESP_WLAN_SPI_SUPPORT;
-#else
-	ESP_LOGI(TAG, "- WLAN over SDIO");
-	cap |= ESP_WLAN_SDIO_SUPPORT;
-#endif
 #ifdef CONFIG_BT_ENABLED
 	cap |= get_bluetooth_capabilities();
 #endif
@@ -147,132 +127,6 @@ static uint8_t get_capabilities()
 	return cap;
 }
 
-static void esp_wifi_set_debug_log()
-{
-	/* set WiFi log level and module */
-#if CONFIG_ESP32_WIFI_DEBUG_LOG_ENABLE
-	uint32_t g_wifi_log_level = WIFI_LOG_INFO;
-	uint32_t g_wifi_log_module = 0;
-	uint32_t g_wifi_log_submodule = 0;
-#if CONFIG_ESP32_WIFI_DEBUG_LOG_DEBUG
-	g_wifi_log_level = WIFI_LOG_DEBUG;
-#endif
-#if CONFIG_ESP32_WIFI_DEBUG_LOG_VERBOSE
-	g_wifi_log_level = WIFI_LOG_VERBOSE;
-#endif
-#if CONFIG_ESP32_WIFI_DEBUG_LOG_MODULE_ALL
-	g_wifi_log_module = WIFI_LOG_MODULE_ALL;
-#endif
-#if CONFIG_ESP32_WIFI_DEBUG_LOG_MODULE_WIFI
-	g_wifi_log_module = WIFI_LOG_MODULE_WIFI;
-#endif
-#if CONFIG_ESP32_WIFI_DEBUG_LOG_MODULE_COEX
-	g_wifi_log_module = WIFI_LOG_MODULE_COEX;
-#endif
-#if CONFIG_ESP32_WIFI_DEBUG_LOG_MODULE_MESH
-	g_wifi_log_module = WIFI_LOG_MODULE_MESH;
-#endif
-#if CONFIG_ESP32_WIFI_DEBUG_LOG_SUBMODULE_ALL
-	g_wifi_log_submodule |= WIFI_LOG_SUBMODULE_ALL;
-#endif
-#if CONFIG_ESP32_WIFI_DEBUG_LOG_SUBMODULE_INIT
-	g_wifi_log_submodule |= WIFI_LOG_SUBMODULE_INIT;
-#endif
-#if CONFIG_ESP32_WIFI_DEBUG_LOG_SUBMODULE_IOCTL
-	g_wifi_log_submodule |= WIFI_LOG_SUBMODULE_IOCTL;
-#endif
-#if CONFIG_ESP32_WIFI_DEBUG_LOG_SUBMODULE_CONN
-	g_wifi_log_submodule |= WIFI_LOG_SUBMODULE_CONN;
-#endif
-#if CONFIG_ESP32_WIFI_DEBUG_LOG_SUBMODULE_SCAN
-	g_wifi_log_submodule |= WIFI_LOG_SUBMODULE_SCAN;
-#endif
-	esp_wifi_internal_set_log_level(g_wifi_log_level);
-	esp_wifi_internal_set_log_mod(g_wifi_log_module, g_wifi_log_submodule, true);
-
-#endif /* CONFIG_ESP32_WIFI_DEBUG_LOG_ENABLE*/
-
-}
-
-void esp_update_ap_mac(void)
-{
-	esp_err_t ret = ESP_OK;
-	char mac_str[BSSID_LENGTH] = "";
-
-	ret = esp_wifi_get_mac(ESP_IF_WIFI_AP, ap_mac);
-	ESP_LOGI(TAG,"Get softap mac address");
-	if (ret) {
-		ESP_LOGE(TAG,"Error in getting MAC of ESP softap %d", ret);
-	} else {
-		snprintf(mac_str,BSSID_LENGTH,MACSTR,MAC2STR(ap_mac));
-		ESP_LOGI(TAG,"AP mac [%s] ", mac_str);
-	}
-}
-
-esp_err_t wlan_ap_rx_callback(void *buffer, uint16_t len, void *eb)
-{
-	interface_buffer_handle_t buf_handle = {0};
-	uint8_t * ap_buf = buffer;
-
-	if (!buffer || !eb || !datapath || ota_ongoing) {
-		if (eb) {
-			esp_wifi_internal_free_rx_buffer(eb);
-		}
-		return ESP_OK;
-	}
-
-	/* Check destination address against self address */
-	if (memcmp(ap_buf, ap_mac, MAC_LEN)) {
-		/* Check for multicast or broadcast address */
-		if (!(ap_buf[0] & 1))
-			goto DONE;
-	}
-
-	buf_handle.if_type = ESP_AP_IF;
-	buf_handle.if_num = 0;
-	buf_handle.payload_len = len;
-	buf_handle.payload = buffer;
-	buf_handle.wlan_buf_handle = eb;
-	buf_handle.free_buf_handle = esp_wifi_internal_free_rx_buffer;
-
-	if (send_to_host_queue(&buf_handle, PRIO_Q_OTHERS))
-		goto DONE;
-
-	return ESP_OK;
-
-DONE:
-	esp_wifi_internal_free_rx_buffer(eb);
-	return ESP_OK;
-}
-
-esp_err_t wlan_sta_rx_callback(void *buffer, uint16_t len, void *eb)
-{
-	// ESP_LOGI(TAG, "%s\n", __func__);
-	interface_buffer_handle_t buf_handle = {0};
-
-	if (!buffer || !eb || !datapath || ota_ongoing) {
-		if (eb) {
-			esp_wifi_internal_free_rx_buffer(eb);
-		}
-		return ESP_OK;
-	}
-
-	buf_handle.if_type = ESP_STA_IF;
-	buf_handle.if_num = 0;
-	buf_handle.payload_len = len;
-	buf_handle.payload = buffer;
-	buf_handle.wlan_buf_handle = eb;
-	buf_handle.free_buf_handle = esp_wifi_internal_free_rx_buffer;
-
-	if (send_to_host_queue(&buf_handle, PRIO_Q_OTHERS))
-		goto DONE;
-
-	return ESP_OK;
-
-DONE:
-	esp_wifi_internal_free_rx_buffer(eb);
-	return ESP_OK;
-}
 
 void process_tx_pkt(interface_buffer_handle_t *buf_handle)
 {
@@ -324,60 +178,6 @@ void parse_protobuf_req(void)
 		r.len, UNKNOWN_CTRL_MSG_ID);
 }
 
-void send_event_to_host(int event_id)
-{
-	protocomm_pserial_data_ready(pc_pserial, NULL, 0, event_id);
-}
-
-void send_event_data_to_host(int event_id, uint8_t *data, int size)
-{
-	protocomm_pserial_data_ready(pc_pserial, data, size, event_id);
-}
-
-void process_serial_rx_pkt(uint8_t *buf)
-{
-	struct esp_payload_header *header = NULL;
-	uint16_t payload_len = 0;
-	uint8_t *payload = NULL;
-	int rem_buff_size;
-
-	header = (struct esp_payload_header *) buf;
-	payload_len = le16toh(header->len);
-	payload = buf + le16toh(header->offset);
-	rem_buff_size = sizeof(r.data) - r.len;
-
-#if CONFIG_ESP_SERIAL_DEBUG
-	ESP_LOG_BUFFER_HEXDUMP(TAG_RX_S, payload, payload_len, ESP_LOG_INFO);
-#endif
-
-	while (r.valid)
-	{
-		ESP_LOGI(TAG,"More segment: %u curr seq: %u header seq: %u\n",
-			header->flags & MORE_FRAGMENT, r.cur_seq_no, header->seq_num);
-		vTaskDelay(10);
-	}
-
-	if (!r.len) {
-		/* New Buffer */
-		r.cur_seq_no = le16toh(header->seq_num);
-	}
-
-	if (header->seq_num != r.cur_seq_no) {
-		/* Sequence number mismatch */
-		r.valid = 1;
-		parse_protobuf_req();
-		return;
-	}
-
-	memcpy((r.data + r.len), payload, min(payload_len, rem_buff_size));
-	r.len += min(payload_len, rem_buff_size);
-
-	if (!(header->flags & MORE_FRAGMENT)) {
-		/* Received complete buffer */
-		r.valid = 1;
-		parse_protobuf_req();
-	}
-}
 
 void process_rx_pkt(interface_buffer_handle_t *buf_handle)
 {
@@ -393,18 +193,8 @@ void process_rx_pkt(interface_buffer_handle_t *buf_handle)
 	ESP_LOG_BUFFER_HEXDUMP(TAG_RX, payload, 8, ESP_LOG_INFO);
 #endif
 
-	if ((buf_handle->if_type == ESP_STA_IF) && station_connected) {
-		/* Forward data to wlan driver */
-		esp_wifi_internal_tx(ESP_IF_WIFI_STA, payload, payload_len);
-		/*ESP_LOG_BUFFER_HEXDUMP("spi_sta_rx", payload, payload_len, ESP_LOG_INFO);*/
-	} else if (buf_handle->if_type == ESP_AP_IF && softap_started) {
-		/* Forward data to wlan driver */
-		esp_wifi_internal_tx(ESP_IF_WIFI_AP, payload, payload_len);
-	} else if (buf_handle->if_type == ESP_SERIAL_IF) {
-		process_serial_rx_pkt(buf_handle->payload);
-	}
 #if defined(CONFIG_BT_ENABLED) && BLUETOOTH_HCI
-	else if (buf_handle->if_type == ESP_HCI_IF) {
+	if (buf_handle->if_type == ESP_HCI_IF) {
 		process_hci_rx_pkt(payload, payload_len);
 	}
 #endif
@@ -524,41 +314,6 @@ static esp_err_t serial_write_data(uint8_t* data, ssize_t len)
 	return ESP_OK;
 }
 
-static esp_err_t initialise_wifi(void)
-{
-	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-	ESP_ERROR_CHECK(esp_event_loop_create_default());
-	esp_err_t result = esp_wifi_init_internal(&cfg);
-	if (result != ESP_OK) {
-		ESP_LOGE(TAG,"Init internal failed");
-		return result;
-	}
-	esp_wifi_set_debug_log();
-	result = esp_supplicant_init();
-	if (result != ESP_OK) {
-		ESP_LOGE(TAG, "Failed to init supplicant (0x%x)", result);
-		esp_err_t deinit_ret = esp_wifi_deinit_internal();
-		if (deinit_ret != ESP_OK) {
-			ESP_LOGE(TAG, "Failed to deinit Wi-Fi internal (0x%x)", deinit_ret);
-			return deinit_ret;
-		}
-		return result;
-	}
-
-	result = esp_wifi_set_mode(WIFI_MODE_NULL);
-	if (result != ESP_OK) {
-		ESP_LOGE(TAG,"Failed to reset wifi mode");
-		return result;
-	}
-
-	result = esp_wifi_start();
-	if (result != ESP_OK) {
-		ESP_LOGE(TAG,"Failed to start WiFi");
-		return result;
-	}
-
-	return result;
-}
 
 int event_handler(uint8_t val)
 {
@@ -721,7 +476,7 @@ void app_main()
 	ret = nvs_flash_init();
 
 	if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
-		ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+	    ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
 		ESP_ERROR_CHECK(nvs_flash_erase());
 		ret = nvs_flash_init();
 	}
@@ -752,73 +507,50 @@ void app_main()
 		}
 	#endif
 
-		pc_pserial = protocomm_new();
-		if (pc_pserial == NULL) {
-			ESP_LOGE(TAG,"Failed to allocate memory for new instance of protocomm ");
-			return;
-		}
+	pc_pserial = protocomm_new();
+	if (pc_pserial == NULL) {
+		ESP_LOGE(TAG,"Failed to allocate memory for new instance of protocomm ");
+		return;
+	}
 
-		/* Endpoint for control command responses */
-		if (protocomm_add_endpoint(pc_pserial, CTRL_EP_NAME_RESP,
-					data_transfer_handler, NULL) != ESP_OK) {
-			ESP_LOGE(TAG, "Failed to add enpoint");
-			return;
-		}
 
-		/* Endpoint for control notifications for events subscribed by user */
-		if (protocomm_add_endpoint(pc_pserial, CTRL_EP_NAME_EVENT,
-					ctrl_notify_handler, NULL) != ESP_OK) {
-			ESP_LOGE(TAG, "Failed to add enpoint");
-			return;
-		}
+	protocomm_pserial_start(pc_pserial, serial_write_data, serial_read_data);
 
-		protocomm_pserial_start(pc_pserial, serial_write_data, serial_read_data);
+	if_context = interface_insert_driver(event_handler);
+	datapath = 1;
 
-		if_context = interface_insert_driver(event_handler);
-		datapath = 1;
+	if (!if_context || !if_context->if_ops) {
+		ESP_LOGE(TAG, "Failed to insert driver\n");
+		return;
+	}
 
-		if (!if_context || !if_context->if_ops) {
-			ESP_LOGE(TAG, "Failed to insert driver\n");
-			return;
-		}
+	if_handle = if_context->if_ops->init();
 
-		if_handle = if_context->if_ops->init();
+	if (!if_handle) {
+		ESP_LOGE(TAG, "Failed to initialize driver\n");
+		return;
+	}
 
-		if (!if_handle) {
-			ESP_LOGE(TAG, "Failed to initialize driver\n");
-			return;
-		}
+	sleep(1);
 
-		sleep(1);
+	/* send capabilities to host */
+	generate_startup_event(capa);
 
-		/* send capabilities to host */
-		generate_startup_event(capa);
+	meta_to_host_queue = xQueueCreate(TO_HOST_QUEUE_SIZE*3, sizeof(uint8_t));
+	assert(meta_to_host_queue);
+	for (prio_q_idx=0; prio_q_idx<MAX_PRIORITY_QUEUES; prio_q_idx++) {
+		to_host_queue[prio_q_idx] = xQueueCreate(TO_HOST_QUEUE_SIZE,
+				sizeof(interface_buffer_handle_t));
+		assert(to_host_queue[prio_q_idx]);
+	}
 
-		meta_to_host_queue = xQueueCreate(TO_HOST_QUEUE_SIZE*3, sizeof(uint8_t));
-		assert(meta_to_host_queue);
-		for (prio_q_idx=0; prio_q_idx<MAX_PRIORITY_QUEUES; prio_q_idx++) {
-			to_host_queue[prio_q_idx] = xQueueCreate(TO_HOST_QUEUE_SIZE,
-					sizeof(interface_buffer_handle_t));
-			assert(to_host_queue[prio_q_idx]);
-		}
-
-		assert(xTaskCreate(recv_task , "recv_task" , 4096 , NULL , 22 , NULL) == pdTRUE);
-		assert(xTaskCreate(send_task , "send_task" , 4096 , NULL , 22 , NULL) == pdTRUE);
-	#ifdef CONFIG_FREERTOS_GENERATE_RUN_TIME_STATS
-		assert(xTaskCreate(task_runtime_stats_task, "task_runtime_stats_task",
-					4096, NULL, 1, NULL) == pdTRUE);
+	assert(xTaskCreate(recv_task , "recv_task" , 4096 , NULL , 22 , NULL) == pdTRUE);
+	assert(xTaskCreate(send_task , "send_task" , 4096 , NULL , 22 , NULL) == pdTRUE);
+#ifdef CONFIG_FREERTOS_GENERATE_RUN_TIME_STATS
+	assert(xTaskCreate(task_runtime_stats_task, "task_runtime_stats_task",
+				4096, NULL, 1, NULL) == pdTRUE);
 	#endif
-
-
-		// tcpip_adapter_init();
-
-		// ESP_ERROR_CHECK(initialise_wifi());
-		// // smartconfig_initialize();
-
 		ESP_LOGI(TAG,"Initial set up done");
-
-		sleep(1);
-		send_event_to_host(CTRL_MSG_ID__Event_ESPInit);
 	} else {
 		ESP_LOGI(TAG, "boot to tm");
 		tm_ble_init();
