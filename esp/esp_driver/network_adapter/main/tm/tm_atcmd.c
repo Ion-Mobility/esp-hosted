310ad1553d5fcde1ee689d420ea0368b23a70662
vult0306 <letuanvu91@gmail.com>
2023-07-03 08:34:17 +0700

    add atcmd handler flow

--- esp/esp_driver/network_adapter/main/tm/tm_atcmd.c
+++ esp/esp_driver/network_adapter/main/tm/tm_atcmd.c
@@ -15,40 +15,36 @@
 #include "tm_atcmd.h"
 
 #define ATCMD_TASK_PRIO     3
+#define ATCMD_CMD_LEN       128
+#define ION_TM_ATCMD_TAG    "TM_ATCMD"
 
 QueueHandle_t atcmd_to_handler_queue;
 QueueHandle_t handler_to_atcmd_queue;
-static void tm_atcmd_process_cmd(void *arg);
+
+static esp_err_t tm_atcmd_recv(char* cmd);
+static esp_err_t tm_atcmd_process(char* cmd, char* res);
+static esp_err_t tm_atcmd_resp(esp_err_t status, char* res);
 
 static void tm_atcmd_task(void *arg)
 {
     esp_err_t ret = ESP_OK;
-
-    WORD_ALIGNED_ATTR char sendbuf[129]="";
-    WORD_ALIGNED_ATTR char recvbuf[129]="";
-    spi_slave_transaction_t t;
-    memset(&t, 0, sizeof(t));
+    WORD_ALIGNED_ATTR char recvbuf[ATCMD_CMD_LEN+1]="";
+    WORD_ALIGNED_ATTR char respbuf[ATCMD_CMD_LEN+1]="";
 
     while(1) {
-        // waiting for command
         memset(recvbuf, 0, sizeof(recvbuf));
-        memset(sendbuf, 0, sizeof(sendbuf));
-        t.length=128*8;
-        t.tx_buffer=sendbuf;
-        t.rx_buffer=recvbuf;
-        ret=spi_slave_transmit(RCV_HOST, &t, portMAX_DELAY);
-        printf("cmd_len: %d\ncmd_data:%s", t.trans_len, recvbuf);
+        ret = tm_atcmd_recv(recvbuf);
+        if (ret != ESP_OK)
+            continue;
 
-        tm_atcmd_process_cmd(NULL);
+        memset(respbuf, 0, sizeof(respbuf));
+        ret = tm_atcmd_process(recvbuf, respbuf);
+        if (ret != ESP_OK)
+            continue;
 
-        t.length=128*8;
-        t.tx_buffer=sendbuf;
-        t.rx_buffer=recvbuf;
-        memset(recvbuf, 0, sizeof(recvbuf));
-        memset(sendbuf, 0, sizeof(sendbuf));
-        snprintf(sendbuf, sizeof(sendbuf), "OK\r\n");
-        ret=spi_slave_transmit(RCV_HOST, &t, portMAX_DELAY);
-        printf("responsed to host\r\n");
+        ret = tm_atcmd_resp(ret, respbuf);
+        if (ret != ESP_OK)
+            continue;
     }
 }
 
@@ -61,7 +57,37 @@ void tm_atcmd_tasks_init(void)
 
 }
 
-static void tm_atcmd_process_cmd(void *arg) {
-    printf("Processing command...\r\n");
+static esp_err_t tm_atcmd_recv(char* cmd) {
+    spi_slave_transaction_t t;
+    memset(&t, 0, sizeof(t));
+    t.length=ATCMD_CMD_LEN*8;
+    t.tx_buffer=NULL;
+    t.rx_buffer=cmd;
+    return spi_slave_transmit(RCV_HOST, &t, portMAX_DELAY);
+}
+
+static esp_err_t tm_atcmd_process(char* cmd, char* res) {
+    ESP_LOGI(ION_TM_ATCMD_TAG, "cmd_data:%s", cmd);
+    // process each command here, timeout is 2s
     vTaskDelay(pdMS_TO_TICKS(1000*2));
+    snprintf(res, ATCMD_CMD_LEN+1, "ion");
+    return ESP_OK;
+}
+
+static esp_err_t tm_atcmd_resp(esp_err_t status, char* res) {
+    spi_slave_transaction_t t;
+    WORD_ALIGNED_ATTR char sendbuf[ATCMD_CMD_LEN+1]="";
+    memset(&t, 0, sizeof(t));
+    t.length=ATCMD_CMD_LEN*8;
+    t.tx_buffer=sendbuf;
+    t.rx_buffer=NULL;
+    memset(sendbuf, 0, sizeof(sendbuf));
+
+    if (status == ESP_OK) {
+        snprintf(sendbuf, sizeof(sendbuf), "OK+%s\r\n",res);
+    } else {
+        snprintf(sendbuf, sizeof(sendbuf), "ERROR+%s\r\n",res);
+    }
+
+    return spi_slave_transmit(RCV_HOST, &t, portMAX_DELAY);
 }
