b1400c67447523e3fac601d63411069d3a5b640e
Kiet Pham <qkiet97@gmail.com>
2023-07-18 09:54:20 +0700

    split app_main of at_cmd_stack test app into smaller functions

--- esp/esp_driver/network_adapter/components/at_cmd_stack/test/main/app_main.c
+++ esp/esp_driver/network_adapter/components/at_cmd_stack/test/main/app_main.c
@@ -4,8 +4,46 @@
 #include "esp_system.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#define INPUT_BUFFER_LENGTH 5
 
-void getLineInput(char *buf, size_t len)
+
+static const char *test_description_table[] =
+{
+    "Restart board",
+    "Test parsing"
+};
+
+
+static void getLineInput(char *buf, size_t len);
+static void printHelper();
+static void parse_and_exec_option(char *option);
+
+void app_main()
+{
+    char chosen_test_option[INPUT_BUFFER_LENGTH];
+    while (1)
+    {
+        printHelper();
+        getLineInput(chosen_test_option, INPUT_BUFFER_LENGTH);
+        parse_and_exec_option(chosen_test_option);
+    }
+}
+
+static void printHelper()
+{
+    printf("---AT command stack test app v0.1---\n\n");
+    printf("Choose your action:\n");
+    int num_of_test_desc = 
+        sizeof(test_description_table) / sizeof(test_description_table[0]);
+    for (int index = 0; index < num_of_test_desc; index++)
+    {
+        printf("'%d': %s\n", index, test_description_table[index]);
+    }
+    printf("\n\n");
+    printf("Your answer is (from '%d' to '%d'): ", 0, num_of_test_desc - 1);
+}
+
+static void getLineInput(char *buf, size_t len)
 {
     memset(buf, 0, len);
     fpurge(stdin); //clears any junk in stdin
@@ -25,6 +63,7 @@ void getLineInput(char *buf, size_t len)
                 else if (*bufp == '\b'){
                     if(bufp-buf >= 1)
                     {
+                        printf("\b \b");
                         bufp--;
                     }
                 }
@@ -44,33 +83,19 @@ void getLineInput(char *buf, size_t len)
         } 
 }
 
-void app_main()
+static void parse_and_exec_option(char *option)
 {
-    char chosen_test_option[3];
-    while (1)
+    printf("\nAnswer is '%s'\n", option);
+    if ((!strcmp(option, "0")) && (strlen(option) == 1))
     {
-        printf("---AT command stack test app v0.1---\n\n");
-        printf("Choose your action:\n");
-        printf("'0': Restart board\n");
-        printf("'1': Test parsing\n");    
-        printf("\n\n");
-        printf("Your answer (from '0' to '1'): ");
-        getLineInput(chosen_test_option, 3);    
-        printf("\nAnswer is '%s'\n", chosen_test_option);
-        if ((!strcmp(chosen_test_option, "0")) && 
-            (strlen(chosen_test_option) == 1))
-        {
-            esp_restart();
-        }
-        if ((!strcmp(chosen_test_option, "1")) && 
-            (strlen(chosen_test_option) == 1))
-        {
-            UNITY_BEGIN();
-            RUN_TEST(TestCase_Parse);
-            UNITY_END();
-        }
-        printf("Not found such option. Please try again\n");
+        esp_restart();
     }
-
-
+    else if ((!strcmp(option, "1")) && (strlen(option) == 1))
+    {
+        UNITY_BEGIN();
+        RUN_TEST(TestCase_Parse);
+        UNITY_END();
+        return;
+    }
+    printf("Not found such option. Please try again\n");
 }
