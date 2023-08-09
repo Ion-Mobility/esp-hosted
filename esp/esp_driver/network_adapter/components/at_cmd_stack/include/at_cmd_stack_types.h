094ac57eee5dedd556fd16d3489a4c78959291dd
Kiet Pham <qkiet97@gmail.com>
2023-08-09 15:01:09 +0700

    increase AT command and response max length to 1500 bytes

--- esp/esp_driver/network_adapter/components/at_cmd_stack/include/at_cmd_stack_types.h
+++ esp/esp_driver/network_adapter/components/at_cmd_stack/include/at_cmd_stack_types.h
@@ -1,8 +1,8 @@
 #ifndef _AT_CMD_STACK_TYPES_H_
 #define _AT_CMD_STACK_TYPES_H_
 
-#define MAX_AT_CMD_LENGTH (200 * sizeof(char))
-#define MAX_AT_RESP_LENGTH (300 * sizeof(char))
+#define MAX_AT_CMD_LENGTH (1500 * sizeof(char))
+#define MAX_AT_RESP_LENGTH (1500 * sizeof(char))
 
 
 #ifndef AT_BUFF_SIZE_T
