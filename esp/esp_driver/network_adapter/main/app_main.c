828fd9ff421f2f510816e54387d4d61e94bc0868
vult0306 <letuanvu91@gmail.com>
2023-12-19 23:04:22 +0700

    remove unused include

--- esp/esp_driver/network_adapter/main/app_main.c
+++ esp/esp_driver/network_adapter/main/app_main.c
@@ -1,18 +1,3 @@
-// Copyright 2015-2021 Espressif Systems (Shanghai) PTE LTD
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -25,26 +10,11 @@
 #ifndef CONFIG_IDF_TARGET_ARCH_RISCV
 #include "xtensa/core-macros.h"
 #endif
-#include "esp_private/wifi.h"
-#include "esp_wpa.h"
-#include "freertos/task.h"
-#include "freertos/queue.h"
-
-#ifdef CONFIG_BT_ENABLED
 #include "esp_bt.h"
-#ifdef CONFIG_BT_HCI_UART_NO
-#include "driver/uart.h"
-#endif
-#endif
-#include "endian.h"
 
-#include "driver/periph_ctrl.h"
 //tm
 #include "tm_ble.h"
 #include "tm_atcmd.h"
-
-#define GPIO_APP_SELECT_PIN	(36)	//148 GPIO_CS
-#define GPIO_INPUT_PIN_SEL  (1ULL<<GPIO_APP_SELECT_PIN)
 
 static const char TAG[] = "ION_BLE";
 
